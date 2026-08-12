72e3119502558a6315ce717b88160c76cea9c0d0
CollinMcGuire1 <CollinMcGuire1@gmail.com>
2026-08-12 13:25:14 -0700

    ch5l1

--- src/main.py
+++ src/main.py
@@ -1,6 +1,20 @@
-from textnode import TextNode, TextType
+import os
+import shutil
 
-def main():
-    new_node = TextNode("dummy text", TextType.BOLD, "dumb URL")
-    print(new_node)
+from copystatic import copy_files_recursive
+
+dir_path_static = "./static"
+dir_path_public = "./public"
+
+
+
+def main() -> None:
+    print("Deleting public directory...")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
+
+    print("Copying static files to public directory")
+    copy_files_recursive(dir_path_static, dir_path_public)
+    
+
 main()
