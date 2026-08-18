6499673714d872d3f17b92bc420ea96976f3631b
Collin McGuire <CollinMcGuire1@gmail.com>
2026-08-18 14:44:43 -0700

    added a local_setup mode that disables IPv6 when running the main.sh localhost server, which does would not work correctly when connected to Tailscale. This setting is toggled back off when the main.sh is finished running

--- main.sh
+++ main.sh
@@ -1,2 +1,12 @@
+#!/bin/bash
+
+# Run local-only setup if it exists (won't exist on other machines/CI)
+if [ -f "./local_setup.sh" ]; then
+    source ./local_setup.sh
+fi
+
+
+
+
 python3 src/main.py
 cd public && python3 -m http.server 8888
